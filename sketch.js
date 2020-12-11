c1ecbeed9fd3c45186e0bd02e7bd82329076b89e
arhmi <miha.arh@telekom.si>
2020-12-11 10:21:50 +0100

    added seriously.js for chroma keying (remove green screen)

--- sketch.js
+++ sketch.js
@@ -2,32 +2,60 @@ let canvas;
 let capture;
 let w = 640;
 let h = 480;
+let chroma;
+let seriously;
 
 function setup() {
-  capture = createCapture(
-    {
-      audio: false,
-      video: {
-        width: { min: 320, ideal: 640, max: 640 },
-        height: { min: 240, ideal: 480, max: 480 },
-      },
-    },
-    function () {
-      console.log("Camera capture ready.");
-    }
-  );
-  capture.size(w, h);
-  capture.id("p5video");
-  capture.elt.setAttribute("playsinline", "");
-  capture.hide();
+  initCamera();
+  initSeriously();
 }
 
 function draw() {
-	drawInitialNosePosition(100, 100)
+	chromaKey();
 }
 
-function drawInitialNosePosition(x, y) {
-	let c = color("magenta");
-	fill(c);
-	ellipse(x, y, 50, 50);
+function initCamera(){
+	capture = createCapture(
+		{
+		  audio: false,
+		  video: {
+			width: { min: 320, ideal: 640, max: 640 },
+			height: { min: 240, ideal: 480, max: 480 },
+		  },
+		},
+		function () {
+		  console.log("Camera capture ready.");
+		}
+	  );
+	  capture.size(w, h);
+	  capture.id("p5video");
+	  capture.elt.setAttribute("playsinline", "");
+	  capture.hide();
+}
+
+function initSeriously() {
+	seriously = new Seriously();
+	
+	var src = seriously.source("#p5video");
+	var target = seriously.target("#p5canvas");
+  
+	chroma = seriously.effect("chroma");
+	chroma.source = src;
+	target.source = chroma;
+  
+	seriously.go();
+}
+
+function chromaKey() {
+	var r = 254 / 255;
+	var g = 255 / 255;
+	var b = 254 / 255;
+	chroma.screen = [r, g, b];
+  
+	chroma.weight = 1;
+	chroma.balance = 1;
+	chroma.clipBlack = 1;
+	chroma.clipWhite = 1;
+  
+	seriously.go();
   }
